a0d3d6318d39a1b198b0b088e1e105686ffee09b
FlexiblyRigid <135572935+FlexiblyRigid@users.noreply.github.com>
2024-02-06 15:09:23 +0800

    Updated Packages

--- bin/config.js
+++ bin/config.js
@@ -1,4 +1,4 @@
 // config for console.re CLI (command line interface) please change YOUR-CHANNEL-NAME to your own unique channel name
-process.env.CHANNEL = 'NodeDefault'
-process.env.SERVER = 'https://console.re'
+process.env.CHANNEL = 'af93-1a21-e4d9'
+process.env.SERVER = 'http://10.61.73.140'
 // if SERVER_PORT is 433 then console.re will use https:// to connect
